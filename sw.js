e1c159bd1a261ae94bc18792d831d3cbecc653b6
Rent-A-Car <56919673+Rent-A-Car@users.noreply.github.com>
2021-06-16 17:10:37 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
-const Static_CACHE = 'static-'+'21-06-16'
+const Static_CACHE = 'static-' + '21-06-16'
 const Static_CACHEAssets = [
-    '/',
-    'https://msng.link/assets/css/bootstrap.min.css',
-    'https://msng.link/assets/css/main.css'
+	'/',
+	'https://msng.link/assets/css/bootstrap.min.css',
+	'https://msng.link/assets/css/main.css'
 ];
 self.addEventListener('install', async event => {
-    self.skipWaiting();
-    const cache = await caches.open(Static_CACHE);
-    await cache.addAll(Static_CACHEAssets);
+	self.skipWaiting();
+	const cache = await caches.open(Static_CACHE);
+	await cache.addAll(Static_CACHEAssets);
 });
 self.addEventListener('activate', async event => {
-    const cachesKeys = await caches.keys();
-    const checkKeys = cachesKeys.map(async key => {
-        if (![Static_CACHE].includes(key)) {
-            await caches.delete(key);
-        }
-    });
-    await Promise.all(checkKeys);
+	const cachesKeys = await caches.keys();
+	const checkKeys = cachesKeys.map(async key => {
+		if (![Static_CACHE].includes(key)) {
+			await caches.delete(key);
+		}
+	});
+	await Promise.all(checkKeys);
 });
 self.addEventListener('fetch', event => {
-    if (event.request.method == "GET") {
-        event.respondWith(async e=>{
-          const cacheS = await caches.open(Static_CACHE);
-          const StaticCachedResponse = await cacheS.match(event.request);
-          return StaticCachedResponse || fetch(event.request);
-        });
-    }
+	if (event.request.method == "GET") {
+		event.respondWith(async e => {
+			const cacheS = await caches.open(Static_CACHE);
+			const StaticCachedResponse = await cacheS.match(event.request);
+			return StaticCachedResponse || fetch(event.request);
+		});
+	}
 });
